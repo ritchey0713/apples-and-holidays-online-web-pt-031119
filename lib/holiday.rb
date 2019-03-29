4b77c9d8bb4b409707d590750155c203bf920629
David Ritchey <ritchey0713@gmail.com>
2019-03-29 22:03:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,23 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
+# holiday_supplies = {
+#   :winter => {
+#     :christmas => ["Lights", "Wreath"],
+#     :new_years => ["Party Hats"]
+#   },
+#   :summer => {
+#     :fourth_of_july => ["Fireworks", "BBQ"]
+#   },
+#   :fall => {
+#     :thanksgiving => ["Turkey"]
+#   },
+#   :spring => {
+#     :memorial_day => ["BBQ"]
+#   }
+# }
 
+def second_supply_for_fourth_of_july(holiday_hash)
+  
   
 
 end

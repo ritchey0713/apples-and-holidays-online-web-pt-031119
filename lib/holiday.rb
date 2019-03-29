d118e3d7b8793c811aee654859dbcf089299b021
David Ritchey <ritchey0713@gmail.com>
2019-03-29 22:17:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,10 +41,10 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  supply = []
   holiday_hash[:winter].each do |holiday, supplies| 
-    binding.pry 
+    supply << supplies 
   end
-
 end
 
 def all_supplies_in_holidays(holiday_hash)

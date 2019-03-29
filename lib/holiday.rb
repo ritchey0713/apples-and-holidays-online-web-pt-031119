54fd58c161178e08b21926c37492d9f7d9b71168
David Ritchey <ritchey0713@gmail.com>
2019-03-29 22:28:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,14 +52,18 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday_name, supplies|
-      puts "  #{holiday_name.to_s.split("_").collect { |element| element.capitalize }.join(' ') }: #{supplies.join(", ")}"
+      puts "  #{holiday_name.to_s.split("_").collect { |element| element.capitalize }.join(' ') }: #{supplies.join(", ") }"
     end 
   end 
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
-
+  holiday_hash.collect do |season, holiday|
+    holiday.collect do |holiday_name, supplies|
+      supplies.include?("BBQ") ? holiday_name : nil 
+    end 
+  end.flatten.compact  
 
 end
 
